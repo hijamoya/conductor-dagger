cc1f224617d7cf612e53ff6f30e5dd6d58df22e0
Tim Rijckaert <tim.rijckaert@vrt.be>
2018-01-05 11:03:51 +0100

    Don't we need the ConductorInjectionModule.class ?

--- app/src/main/java/com/christianbahl/conductor/dagger/sample/activity/MainActivityComponent.java
+++ app/src/main/java/com/christianbahl/conductor/dagger/sample/activity/MainActivityComponent.java
@@ -1,12 +1,17 @@
 package com.christianbahl.conductor.dagger.sample.activity;
 
+import com.christianbahl.conductor.ConductorInjectionModule;
 import com.christianbahl.conductor.dagger.sample.di.binder.ControllerBindingModule;
 import com.christianbahl.conductor.dagger.sample.di.scope.ActivityScope;
 
 import dagger.Subcomponent;
 import dagger.android.AndroidInjector;
 
-@Subcomponent(modules = {MainActivityModule.class, ControllerBindingModule.class})
+@Subcomponent(modules = {
+        MainActivityModule.class,
+        ControllerBindingModule.class,
+        ConductorInjectionModule.class
+})
 @ActivityScope
 public interface MainActivityComponent extends AndroidInjector<MainActivity> {
     @Subcomponent.Builder
